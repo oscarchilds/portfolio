a574c870741c551310ce377d28f37a706a720b1e
Oscar Childs <oscarjchilds@gmail.com>
2025-07-20 17:00:18 +0100

    WIP homepage

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,44 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/projects">Projects</RouterLink>
+  </nav>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <div id="splitter" />
 
   <RouterView />
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
+<style scoped lang="scss">
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  width: 150px;
+  gap: 15px;
+  flex-shrink: 0;
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
+  a {
+    color: black;
+    text-decoration: none;
+    margin-right: 10px;
+    font-size: 1.5em;
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
+    &.router-link-active {
+      font-weight: bold;
+    }
   }
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+#splitter {
+  width: 2px;
+  background-color: #a5a5a5;
+  height: 90vh;
+  margin-right: 50px;
+  flex-shrink: 0;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <h1>Hi, my name is Oscar Childs</h1>
+    <p>I am a Software Developer based in South Derbyshire, UK</p>
+    <p>
+      I am a Full Stack developer with 6+ years of commercial experience. My main strengths are
+      C#/.NET, VueJS, Typescript, SQL Server, creating public and private resuable code libraries
+      via Nuget or NPM, and creating Azure infrastructre and CI/CD pipelines. I also have experience
+      with some experience Java and Blazor.
+    </p>
+    <p>
+      I like to try and get involved in open-source projects. The
+      <RouterLink to="/projects">Projects</RouterLink> page contains information about some of my
+      own projects that I've worked on and published publicly, as well as some of the open-source
+      projects I've contributed to.
+    </p>
+    <p>
+      You can also find me on
+      <a href="https://github.com/oscarchilds" target="_blank">GitHub</a> and
+      <a href="https://www.linkedin.com/in/oscar-childs-4b5843146/" target="_blank">LinkedIn</a>.
+    </p>
+    <p>
+      I am a husband and father of two, and I enjoy spending my free time with my family as much as
+      I can, as well as playing video games or watching films or motor racing.
+    </p>
   </main>
 </template>
+
+<style scoped lang="scss">
+h1 {
+  font-size: 3em;
+  margin-bottom: 0.5em;
+}
+</style>
